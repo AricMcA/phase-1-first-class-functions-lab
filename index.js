652c56398bf279ee2ab4f33572764ad4b3c753b6
Aric McAlister <aricmca@gmail.com>
2021-06-10 18:57:08 -0500

    commit tree b157e9c83274e5546278ce348566b1802131f446

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function() {
+    const newArray = [...drivers];
+    newArray.splice(2,3);
+    return newArray;
+}
+
+const returnLastTwoDrivers = function() {
+    const newArray = [...drivers];
+    newArray.splice(0,2);
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
